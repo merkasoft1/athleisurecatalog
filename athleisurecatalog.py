c41d50069fab731612a962525a034a1efefec09f
merkasoft1 <144548574+merkasoft1@users.noreply.github.com>
2023-09-10 17:06:17 -0400

    Update athleisurecatalog.py

--- athleisurecatalog.py
+++ athleisurecatalog.py
@@ -2,7 +2,7 @@ import streamlit
 import snowflake.connector
 import pandas
 streamlit.title('Merk\'s Not So Amazing Athleisure Catalog')
-streamlit.text('It's the datalake that is the cool part!')
+streamlit.text('It\'s the datalake that is the cool part!')
 # connect to snowflake
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
